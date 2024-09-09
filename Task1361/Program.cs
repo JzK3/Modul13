526dbd70a43f4fffd1a49b359ca63a6f8e2566bd
JzK <andre-kiryushin@mail.ru>
2024-09-09 12:49:22 +1000

    Добавьте файлы проекта.

---
+++ Task1361/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Task1361
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            var text = File.ReadAllText(@"C:\Users\JzK\Desktop\azaz.txt");
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = text.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+            List<string> list = new List<string>();
+            var stopWatch1 = Stopwatch.StartNew();
+            foreach (var item in words)
+            {
+                list.Add(item);
+            }
+            Console.WriteLine("time 1");
+            Console.WriteLine(stopWatch1.Elapsed.TotalMilliseconds);
+            LinkedList<string> linkedList = new LinkedList<string>();
+            var stopWatch = Stopwatch.StartNew();
+            foreach (var item in words)
+            {
+                linkedList.AddLast(item);
+            }
+            Console.WriteLine("time 2");
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
